54d303ff7dd53a18492046767f5d12f0777bdf27
bla bla <rnbkan5@outlook.com>
2023-01-13 20:05:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace exe_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable declaration
+            float workHours = 0, missedHours = 0, hourPay = 0, totalPay = 0;
+
+            //recieving input
+            Console.WriteLine("Enter hours worked:");
+            workHours = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter hours missed:");
+            missedHours= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter pay per hour:");
+            hourPay= int.Parse(Console.ReadLine());
+
+            //calculating salary without bonus
+            totalPay = hourPay * workHours;
+
+            //adding or not adding 10% bonus
+            if (missedHours<3.5)
+            {
+                totalPay += totalPay / 10;
+            }
+
+            //printing salary
+            Console.WriteLine($"The salary is: {totalPay}");
+
+        }
+    }
+}
